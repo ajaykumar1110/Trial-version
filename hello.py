dd22a31a8a4bbe180a47da27030fd8686a49981b
ajaykumar1110 <36915199+ajaykumar1110@users.noreply.github.com>
2020-09-21 20:07:09 +0530

    Added first file on git

---
+++ hello.py
@@ -0,0 +1,13 @@
+# a,b=0,1
+# while(b<1000):
+# 	print(b,end=" ",flush=True)
+# 	a,b=b,a+b
+# print()
+# basket=[1,2,3,4,5]
+# new_basket=basket.append(100)
+# print(basket)
+
+intr=input("ener")
+basket=[]
+basket.append(intr)
+print(basket)
